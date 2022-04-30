bb6ab0ab39ded6635bc64a43fbba802c772a2eaf
joy14 <joy14479@gmail.com>
2022-04-30 20:19:01 +0900

    최종버전, 이클립스에 넣어서 확인해볼것

--- src/HW2.java
+++ src/HW2.java
@@ -1,6 +1,46 @@
 import java.io.*;
+import java.sql.Array;
 import java.util.*;
 
+class User implements Comparable<User> {
+    int name;
+    float jacc;
+
+    public User(float jacc, int name){
+        this.jacc = jacc;
+        this.name = name;
+    }
+
+    @Override
+    public int compareTo(User user) {
+       if(user.jacc > jacc) {
+           return 1;
+       } else if(user.jacc < jacc) {
+           return -1;
+       }
+       return 0;
+    }
+}
+
+class itemPair implements Comparable<itemPair> {
+    int name;
+    int frequency;
+
+    public itemPair(int frequency, int name){
+        this.name = name;
+        this.frequency = frequency;
+    }
+
+    @Override
+    public int compareTo(itemPair it) {
+        if(it.frequency > frequency) {
+            return 1;
+        } else if(it.frequency < frequency) {
+            return -1;
+        }
+        return 0;
+    }
+}
 public class HW2 {
         static String userName;
         static int originNumber;
@@ -52,8 +92,9 @@ public class HW2 {
              return list;
         }
 
-        public static TreeMap<Float, Integer> getJaccard(ArrayList<HashSet<Integer>> list, int userNumber) {
-            TreeMap<Float, Integer> jaccard = new TreeMap<>(Collections.reverseOrder());
+        public static ArrayList<User> getJaccard(ArrayList<HashSet<Integer>> list, int userNumber) {
+            ArrayList<User> jaccard = new ArrayList<>();
+
             HashSet<Integer> origin = list.get(userNumber - 1);
             int size = origin.size();
             float jacc = 0;
@@ -69,24 +110,64 @@ public class HW2 {
                         intersection++;
 
                 jacc = intersection / (float)(size + comparator.size() - intersection);
-                jaccard.put(jacc, i + 1);
+                jaccard.add(new User(jacc, i + 1));
             }
+            Collections.sort(jaccard);
             return jaccard;
         }
         public static void main(String args[]) {
+            System.out.print("파일 이름, 사용자 이름, 사용자 수, 항목 수? ");
 
             ArrayList<HashSet<Integer>> list = getList();
-            TreeMap<Float, Integer> jaccard = getJaccard(list, originNumber);
+            ArrayList<User> jaccard = getJaccard(list, originNumber);
 
-            Iterator<Float> keys = jaccard.keySet().iterator();
-            Iterator<Integer> values = jaccard.values().iterator();
-            while(keys.hasNext()){
-                float key = keys.next();
-                int value = values.next();
-                System.out.println(key + " " + value);
+            ArrayList<HashSet<Integer>> top = new ArrayList<>();
+            User user;
+            Iterator<User> it = jaccard.iterator();
+            HashSet<Integer> origin = list.get(originNumber - 1);
+            for(int i = 0; i < userNumber; i++) {
+                user = it.next();
+                HashSet<Integer> temp = list.get(user.name - 1);
+                Iterator<Integer> iter = origin.iterator();
+                while(iter.hasNext()){
+                    temp.remove(iter.next());
+                }
+
+                top.add(temp);
             }
 
-        }
+            HashMap<Integer, Integer> item = new HashMap<>();
+            Iterator<HashSet<Integer>> iter = top.iterator();
+            HashSet<Integer> eachList;
+            while(iter.hasNext()){
+                eachList = iter.next();
 
+                Iterator<Integer> iterator = eachList.iterator();
+                int next;
+                while(iterator.hasNext()){
+                    next =  iterator.next();
+                    if(!item.containsKey(next))
+                        item.put(next, 1);
+                    else {
+                        item.put(next, item.get(next) + 1);
+                    }
+                }
+            }
+
+            ArrayList<itemPair> pair = new ArrayList<>();
+            Iterator<Integer> keyIt = item.keySet().iterator();
+            Iterator<Integer> valueIt = item.values().iterator();
+            while(keyIt.hasNext()) {
+                pair.add(new itemPair(valueIt.next(), keyIt.next()));
+            }
+            Collections.sort(pair);
+
+            System.out.print("결과: ");
+            Iterator<itemPair> ansIt = pair.iterator();
+            while(itemNumber-- != 0){
+                itemPair temp = ansIt.next();
+                System.out.print(temp.name + "(" + temp.frequency + ") ");
+            }
+        }
 
 }
